d273ec82b1b7a1fdd8ab377fbb703ed4e6edeb1b
Develop and System <developandsystem@gmail.com>
2022-06-07 09:38:19 +0200

    update

---
+++ 01_inicio/utils/js/estructuras.js
@@ -0,0 +1,50 @@
+// if if else switch while do while for foreach (especial) for of for on break goto
+
+// if if else
+let acierto = true;
+
+/*if (acierto) {
+  console.log("acierto verdadero");
+}*/
+/* if (acierto) {
+  console.log("La respuesta es true");
+} else if () {
+     
+} else if () {
+    
+} else {
+    
+} */
+
+/* let nota = 4;
+if (nota >= 5) {
+  console.log("El examen está aprobado");
+} else {
+  console.log("El examen no está aprobado");
+} */
+
+// if ternario
+// condicion ? true : false
+
+// acierto ? console.log("examen aprobado") : console.log("examen suspenso");
+// if else
+// if else if else
+
+// if
+// acierto & console.log("examen aprobado");
+
+let nota = 3;
+switch (nota) {
+  case 1:
+    console.log("dato introducido 1");
+    break;
+  case 2:
+    console.log("dato introducido 2");
+    break;
+  case 3:
+    console.log("dato introducido 3");
+    break;
+  default:
+    console.log("dato incorrecto");
+    break;
+}
