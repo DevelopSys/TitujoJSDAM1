a297fb16973f3e9d79c4f3b715d4804cedaa2d43
Develop and System <developandsystem@gmail.com>
2022-06-07 10:16:45 +0200

    Update estructuras.js

--- 01_inicio/utils/js/estructuras.js
+++ 01_inicio/utils/js/estructuras.js
@@ -48,3 +48,58 @@ switch (nota) {
     console.log("dato incorrecto");
     break;
 }
+
+/*
+Pedir por prompt dos numeros
+Pedir por prompt una operacions (suma, resta, division, multiplicacion)
+
+- si se introduce números menores que 0 o letras que salte una alerta 
+indicando el error y el programa parará
+- Si los numeros introducidos son validos se realizará la operacion indicada
+
+- si se detecta que la operaciones es negativa el programa parará 
+tras realizar todas las operaciones de los números y saltará una alerta
+indicando de ello
+*/
+
+let numero1 = prompt("Introduce operador uno");
+let numero2 = prompt("Introduce operador dos");
+
+if (
+  !isNaN(Number(numero1)) &&
+  !isNaN(Number(numero2)) &&
+  numero1 > 0 &&
+  numero2 > 0
+) {
+  let operacion = prompt("Introduce la operacion a realizar");
+  let resultado;
+  switch (operacion) {
+    case "suma":
+      resultado = numero1 + numero2;
+      break;
+    case "resta":
+      resultado = numero1 - numero2;
+
+      break;
+    case "multiplicacion":
+      resultado = numero1 * numero2;
+
+      break;
+    case "division":
+      resultado = numero1 / numero2;
+
+      break;
+
+    default:
+      console.log("operacion desconocida");
+      break;
+  }
+  resultado > 0
+    ? alert(`La ${operacion} entre ${numero1} y ${numero2} es ${resultado}`)
+    : alert("resultado negativo");
+} else {
+  console.log("Alguno de los datos son incorrectos");
+}
+
+//let numeroTraducido = (Number)numero
+isNaN(Number(numero1)) ? console.log("es un NAN") : console.log("es un numero");
