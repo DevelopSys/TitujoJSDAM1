51676542790fc56f9c0a723c1592389545a42aa8
Develop and System <developandsystem@gmail.com>
2022-06-07 11:29:48 +0200

    Update cadenas.js

--- 01_inicio/utils/js/cadenas.js
+++ 01_inicio/utils/js/cadenas.js
@@ -1,7 +1,26 @@
 let frase = "Esto es un ejemplo de string";
+frase = frase.replaceAll(" ", "");
 console.log(frase);
+let vocales = 0,
+  consonantes = 0;
+
 for (let index = 0; index < frase.length; index++) {
-  console.log(frase.charAt(index).toLowerCase());
+  if (
+    frase.charAt(index).toLowerCase() == "a" ||
+    frase.charAt(index).toLowerCase() == "e" ||
+    frase.charAt(index).toLowerCase() == "i" ||
+    frase.charAt(index).toLowerCase() == "o" ||
+    frase.charAt(index).toLowerCase() == "u"
+  ) {
+    vocales++;
+  } else {
+    consonantes++;
+  }
 }
 
+alert(
+  `El numero de vocales es ${vocales} y el de consonantes es ${consonantes}`
+);
+
 // sacar por consola cada una de las letras que tiene la palabra
+// sacar el número de vocales y consonantes por un alert
