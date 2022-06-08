68c4af27da296fb2fe2a527e85533c8532448986
Develop and System <developandsystem@gmail.com>
2022-06-08 09:43:03 +0200

    update

--- 01_inicio/utils/js/cadenas.js
+++ 01_inicio/utils/js/cadenas.js
@@ -24,3 +24,33 @@ alert(
 
 // sacar por consola cada una de las letras que tiene la palabra
 // sacar el número de vocales y consonantes por un alert
+
+let frase = "Esto es un ejemplo de frase para analizar";
+let estaLetra = frase.includes("E");
+let subFrase = frase.slice(0, 5);
+// se pida por pronpt un correo electronico
+// se compruebe si está bien formateado:
+// tiene un @
+// en la segunda parte tiene un .
+// tiene una extension com o es
+// borja@gmail.com
+// borja@borja@gmail.es
+// borja@gmail.asd
+// borja.martin@gmail.com
+// console.log(subFrase);
+
+let correo = prompt("Por favor introduce un correo electronico");
+let numeroArrobas = 0;
+let posicionArroba = 0;
+for (let index = 0; index < correo.length; index++) {
+  if (correo.charAt(index) == "@") {
+    numeroArrobas++;
+    posicionArroba = index;
+  }
+}
+
+if (numeroArrobas == 1) {
+  // borja@gmail.com
+  let dominio = correo.slice(posicionArroba + 1, correo.length);
+  console.log(dominio);
+}
