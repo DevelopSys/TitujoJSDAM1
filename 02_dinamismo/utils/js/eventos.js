2df236fb83121d0d2803283f273089e304f54f91
Develop and System <developandsystem@gmail.com>
2022-06-13 09:38:34 +0200

    update

--- 02_dinamismo/utils/js/eventos.js
+++ 02_dinamismo/utils/js/eventos.js
@@ -4,7 +4,19 @@ let contador = 0;
 let spanPulsaciones = document.querySelector("span");
 
 boton_agregar.addEventListener("click", (element) => {
-  console.log(element);
+  let contenido = input_agregar.value;
+  // console.log(contenido);
+  // agregar nodos
+  /*let lista = document.querySelector("#lista_primeros");
+  let nodoLI = document.createElement("li");
+  nodoLI.innerText = contenido;
+  //nodoLI.classList.add("list-group-item");
+  nodoLI.setAttribute("class", "list-group-item");
+  nodoLI.setAttribute("id", "valor");
+  lista.appendChild(nodoLI);*/
+  // modificar el html
+  let lista = document.querySelector("#lista_primeros");
+  lista.innerHTML = `${lista.innerHTML} <li class='list-group-item'>${contenido}</li>`;
 });
 
 input_agregar.addEventListener("keydown", (event) => {
@@ -15,8 +27,8 @@ input_agregar.addEventListener("keydown", (event) => {
   spanPulsaciones.innerText = contador;
 });
 
-// el en parrafo que está configurado al final en el 0
-// deberá aparecer el número de pulsaciones realizadas (dinámico)
+/*  el en parrafo que está configurado al final en el 0
+ deberá aparecer el número de pulsaciones realizadas (dinámico)
 
-// declarar la funcion fuera (va a ser reutilizada?)
-// o dentro(solo ejecutada por el evento) --> anónima
+ declarar la funcion fuera (va a ser reutilizada?)
+ o dentro(solo ejecutada por el evento) --> anónima */
