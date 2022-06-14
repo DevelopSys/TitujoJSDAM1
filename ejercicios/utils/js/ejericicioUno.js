e6c0c790f5212338a52aa97846831138563e2e05
Develop and System <developandsystem@gmail.com>
2022-06-14 09:41:16 +0200

    update

---
+++ ejercicios/utils/js/ejericicioUno.js
@@ -0,0 +1,31 @@
+let inputTweet = document.querySelector("input");
+let textoPulsaciones = document.querySelector("#tamanio_texto");
+let botonMandar = document.querySelector("button");
+let divTweets = document.querySelector(".col-8 .row");
+
+let contador = 0;
+inputTweet.addEventListener("keyup", () => {
+  // modificar el contenido de texto con un contador
+  //contador++;
+  textoPulsaciones.innerText = `${inputTweet.value.length}/255`;
+});
+botonMandar.addEventListener("click", () => {
+  // capturar el input y meterlo en una card (que tendrá que estar en una col y en el div asociado)
+
+  let contenido = inputTweet.value;
+  if (contenido.length == 0 || contenido.length > 255) {
+    alert("Tweet invalido, por favor revisalo");
+  } else {
+    // anadir una carta al divTweets
+    divTweets.innerHTML = `${divTweets.innerHTML} <div class="col">
+              <div class="card" style="width: 18rem">
+                <div class="card-body">
+                  <h5 class="card-title">Tweet </h5>
+                  <p class="card-text">
+                    ${contenido}
+                  </p>
+                </div>
+              </div>
+            </div>`;
+  }
+});
