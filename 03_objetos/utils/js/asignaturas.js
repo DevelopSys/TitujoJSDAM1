535d44cd27f35b6f50f56420b0559e588a63d9f3
Develop and System <developandsystem@gmail.com>
2022-06-16 10:31:03 +0200

    Update asignaturas.js

--- 03_objetos/utils/js/asignaturas.js
+++ 03_objetos/utils/js/asignaturas.js
@@ -4,34 +4,35 @@ let asignaturas = {
       nombre: "Programacion",
       horas: 8,
       curso: 1,
-      imgen:
+      imagen:
         "https://www.programmerszone.ae/wp-content/uploads/2021/10/programming-evolution.jpg",
     },
     {
       nombre: "Base datos",
       horas: 6,
       curso: 1,
-      imgen: "http://blog.cloudflare.com/content/images/2022/05/image1-22.png",
+      imagen: "http://blog.cloudflare.com/content/images/2022/05/image1-22.png",
     },
     {
       nombre: "Lenguaje Marcas",
       horas: 5,
       curso: 1,
-      imgen:
+      imagen:
         "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
     },
     {
       nombre: "Programación multimedia",
       horas: 4,
       curso: 2,
-      imgen:
+      imagen:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3vK5eZst_syxgKyGBKSl7odXXg1E49IlIg&usqp=CAU",
     },
     {
       nombre: "Desarrollo interfaces",
       horas: 6,
       curso: 2,
-      imgen: "https://sm-services.es/wp-content/uploads/2021/02/MEAN-Stack.jpg",
+      imagen:
+        "https://sm-services.es/wp-content/uploads/2021/02/MEAN-Stack.jpg",
     },
   ],
   numero: 2,
